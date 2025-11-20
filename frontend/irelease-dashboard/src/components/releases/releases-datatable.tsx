a6b733d217158724bdd302b9d7fa1cd46e97a13a
kelvinnmuia <kelvinnmuia@gmail.com>
2025-11-20 18:32:25 +0300

    added order by date functionality to the datatable

--- frontend/irelease-dashboard/src/components/releases/releases-datatable.tsx
+++ frontend/irelease-dashboard/src/components/releases/releases-datatable.tsx
@@ -606,7 +606,7 @@ export function ReleasesDataTable() {
             </DropdownMenu>
 
             {/* Search Input */}
-            <div className="relative flex-1 lg:flex-none lg:w-62">
+            <div className="relative flex-1 lg:flex-none lg:w-64">
               <Search className="absolute left-3 top-1/2 transform -translate-y-1/2 h-5 w-5 text-gray-500 pointer-events-none" />
               <Input
                 placeholder="Search releases..."
